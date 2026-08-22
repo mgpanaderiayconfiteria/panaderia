3617511e8e4814e94c8fb25d3bfa281631d9a3a0
alastorhernan <alastorhernan@gmail.com>
2026-08-22 17:58:34 -0300

    Actualizacion automatica

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-// 1. IMPORTAR GOOGLE ANALYTICS
-import ReactGA from 'react-ga4';
-
 // Contextos activos
 import { AuthProvider } from './context/AuthContext';
+import { ProductProvider } from './context/ProductContext';
 import { CartProvider } from './context/CartContext';
-
-// 2. INICIALIZAR ANALYTICS (Reemplazá G-XXXXXXXXXX por tu ID real)
-ReactGA.initialize('G-ZR6R1Z104T');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
+    <AuthProvider>
+      <ProductProvider>
         <CartProvider>
           <App />
         </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+      </ProductProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
